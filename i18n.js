3ae88187bdc49b57b474b549339a3bd57ad2ffe9
Max Mitschke <max@movact.de>
2021-06-13 21:33:37 +0200

    finnish language

--- i18n.js
+++ i18n.js
@@ -1,6 +1,5 @@
 module.exports = {
-  //"locales": ['de', 'en', 'sv', 'fr', 'fi'],
-  locales: ['de', 'en', 'sv', 'fr'],
+  locales: ['de', 'en', 'sv', 'fr', 'fi'],
   defaultLocale: 'de',
   pages: {
     '*': ['common', 'header', 'footer'],
