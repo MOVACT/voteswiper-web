77f6cc9f98b99f60d02422f596f6cd76cd81147f
Max Mitschke <max@movact.de>
2021-08-09 17:23:09 +0200

    Enable Source Code Links

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -1,5 +1,5 @@
 import IconFacebook from 'icons/facebook.svg';
-//import IconGithub from 'icons/github.svg';
+import IconGithub from 'icons/github.svg';
 import IconInstagram from 'icons/instagram.svg';
 import IconTwitter from 'icons/twitter.svg';
 import useTranslation from 'next-translate/useTranslation';
@@ -32,7 +32,7 @@ export const socialLinks = [
   },
 ];
 
-/*const openSourceLinks = [
+const openSourceLinks = [
   {
     href: 'https://github.com/movact/voteswiper-app',
     translatedLink: false,
@@ -54,7 +54,7 @@ export const socialLinks = [
     translatedText: true,
     icon: IconGithub,
   },
-];*/
+];
 
 const moreLinks = [
   {
@@ -166,10 +166,10 @@ const Footer: React.FC = () => {
               >
                 Source Code
               </div>
-              {/*<nav aria-labelledby="openSource">
+              <nav aria-labelledby="openSource">
                 <ul>{openSourceLinks.map((item) => renderLink(item))}</ul>
-  </nav>*/}
-              <p className="pt-1">{t('footer:sourceCodeNote')}</p>
+              </nav>
+              {/*<p className="pt-1">{t('footer:sourceCodeNote')}</p>*/}
             </div>
             <div className="w-full px-4 pt-6 md:w-1/2 lg:w-1/4 lg:pt-0">
               <div
