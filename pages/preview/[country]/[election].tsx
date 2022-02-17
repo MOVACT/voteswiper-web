3e98d51caea42a8731e88246ef4ae1d50c0191f6
Max Mitschke <max@movact.de>
2022-02-17 08:47:01 +0100

    fix build

--- pages/preview/[country]/[election].tsx
+++ pages/preview/[country]/[election].tsx
@@ -150,7 +150,6 @@ const ElectionPreviewPage: NextPage<Props> = ({
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export const getServerSideProps: GetServerSideProps = async ({
-  preview,
   params,
   locale,
 }) => {
